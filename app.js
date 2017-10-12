28553260de7bb29aeb9752c9074a6871473f252f
biswarup <bis.banerjee.bb@gmail.com>
2017-10-12 12:47:10 +0530

    done with auto suggestion

--- app.js
+++ app.js
@@ -15,11 +15,15 @@ function fireAJAX()
         console.log(items[1]);
         console.log("JSON OBJ");
         // console.log(jsonobject);
-        console.log("we are from another worls" + items[0].CODE);
+        console.log("we are from another worls " + items[0].CODE);
         console.log("availableTags: " + availableTags);
         availableTags.push("HELLO EARTH");
         console.log("after availableTags: " + availableTags);
-
+        for(var i=0;i<7000;i++)
+        {
+        	availableTags.push(items[i].CODE);
+        }
+        console.log("availableTags: " + availableTags);
 
         // alert(jsonobject);
         // $('#appendHere').append(jsonobject);
@@ -31,36 +35,40 @@ function fireAJAX()
     }
 });
 }
+    
+
 
 console.log(jsonobject);
   $( function() {
-     availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    availableTags.push("HELLO WORLD");
+    //  availableTags = [
+    //   "ActionScript",
+    //   "AppleScript",
+    //   "Asp",
+    //   "BASIC",
+    //   "C",
+    //   "C++",
+    //   "Clojure",
+    //   "COBOL",
+    //   "ColdFusion",
+    //   "Erlang",
+    //   "Fortran",
+    //   "Groovy",
+    //   "Haskell",
+    //   "Java",
+    //   "JavaScript",
+    //   "Lisp",
+    //   "Perl",
+    //   "PHP",
+    //   "Python",
+    //   "Ruby",
+    //   "Scala",
+    //   "Scheme"
+    // ];
+    // availableTags.push("HELLO EARTH");
 
     $( "#searchStock" ).autocomplete({
+    	max:10,
+     	minLength:3,
     	source: availableTags
     });
   } );
